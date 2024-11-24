48e722df43aa8cf96f3e270b460aec2065c59161
Gregory-Unknown <57668110+Gregory-Unknown@users.noreply.github.com>
2024-11-24 20:26:24 +0300

    FEATURE // hw11

--- src/main/java/org/example/HomeWork.java
+++ src/main/java/org/example/HomeWork.java
@@ -5,6 +5,10 @@ import lombok.SneakyThrows;
 
 import java.io.InputStream;
 import java.io.OutputStream;
+import java.io.PrintWriter;
+import java.util.Arrays;
+import java.util.Scanner;
+
 
 public class HomeWork {
 
@@ -14,11 +18,34 @@ public class HomeWork {
      */
     @SneakyThrows
     public void stepDanceValue(InputStream in, OutputStream out) {
-        out.write('3');
-        out.write('\n');
-        out.flush();
+        Scanner scanner = new Scanner(in);
+        PrintWriter writer = new PrintWriter(out);
+
+        int N = scanner.nextInt();
+        int Q = scanner.nextInt();
+
+        char[] choreography = new char[N];
+        Arrays.fill(choreography, 'L');
+
+        for (int i = 0; i < Q; i++) {
+            int position = scanner.nextInt() - 1;
+
+            choreography[position] = (choreography[position] == 'L') ? 'R' : 'L';
+
+            int longestValue = 1;
+            for (int j = 1; j < N; j++) {
+                if (choreography[j] != choreography[j - 1]) {
+                    longestValue++;
+                }
+            }
+
+            writer.print(longestValue);
+            if (i + 1 < Q) {
+                writer.println();
+            }
+        }
+
+        writer.flush();
+        scanner.close();
     }
-
-
-
 }
