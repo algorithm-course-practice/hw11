7eb29b096aed7eba1584782c359bf5f5b7092683
Alecos-Sad <alecos100@gmail.com>
2024-11-27 17:53:36 +0300

    solution dummy

--- src/main/java/org/example/HomeWork.java
+++ src/main/java/org/example/HomeWork.java
@@ -3,8 +3,14 @@ package org.example;
 
 import lombok.SneakyThrows;
 
-import java.io.InputStream;
-import java.io.OutputStream;
+import java.io.*;
+import java.nio.charset.StandardCharsets;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Scanner;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
 
 public class HomeWork {
 
@@ -14,11 +20,47 @@ public class HomeWork {
      */
     @SneakyThrows
     public void stepDanceValue(InputStream in, OutputStream out) {
-        out.write('3');
-        out.write('\n');
-        out.flush();
+        Scanner sc = new Scanner(in);
+
+        int n = sc.nextInt();
+        int q = sc.nextInt();
+        char[] chars = new char[n];
+        for (int i = 0; i < n; i++) {
+            chars[i] = 'L';
+        }
+
+        int choreographyValue = 1;
+        int[] diffs = new int[n - 1];
+        for (int i = 0; i < n - 1; i++) {
+            diffs[i] = (chars[i] != chars[i + 1]) ? 1 : 0;
+            choreographyValue += diffs[i];
+        }
+
+        StringBuilder output = new StringBuilder();
+
+        for (int k = 0; k < q; k++) {
+            int pos = sc.nextInt() - 1;
+
+            chars[pos] = (chars[pos] == 'L') ? 'R' : 'L';
+
+            if (pos > 0) {
+                int oldDiff = diffs[pos - 1];
+                diffs[pos - 1] = (chars[pos - 1] != chars[pos]) ? 1 : 0;
+                choreographyValue += diffs[pos - 1] - oldDiff;
+            }
+            if (pos < n - 1) {
+                int oldDiff = diffs[pos];
+                diffs[pos] = (chars[pos] != chars[pos + 1]) ? 1 : 0;
+                choreographyValue += diffs[pos] - oldDiff;
+            }
+
+            output.append(choreographyValue);
+            if (k < q - 1) {
+                output.append("\n");
+            }
+        }
+
+        out.write(output.toString().replaceAll("\n", "").replaceAll("\r", "").getBytes());
     }
-
-
 
 }
